da4f4ba2163e5e282bb780f07d3a550f32370103
С - 13 < - 13@DESKTOP-NDUUGHD>
2024-02-21 16:34:10 +0500

    Добавьте файлы проекта.

---
+++ задача/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+    
+int main()
+{   
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	int abc;
+
+	do
+	{
+		cin >> abc;
+	
+	
+	} while (abc != 12);
+
+	cout << "Вы Молодец!\n";
+
+
+
+	return 0;
+}  
